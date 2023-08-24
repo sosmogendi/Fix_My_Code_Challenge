e4df805bf0a6afc14646af3df63473f58c7fa05f
Sospeter <sosmogendi@gmail.com>
2023-08-24 07:55:28 -0700

    Sorting arguments in Ruby

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,32 +1,31 @@
 ###
-# Sort integer arguments (ascending)
+#
+#  Sort integer arguments (ascending) 
+#
 ###
 
 result = []
-
 ARGV.each do |arg|
     # skip if not integer
-    next unless arg =~ /^-?\d+$/
+    next if arg !~ /^-?[0-9]+$/
 
     # convert to integer
     i_arg = arg.to_i
-
+    
     # insert result at the right position
     is_inserted = false
     i = 0
     l = result.size
-
     while !is_inserted && i < l do
-        if result[i] <= i_arg
+        if result[i] < i_arg
             i += 1
         else
-            result.insert(i, i_arg)
+            result.insert(i, i_arg)  # Corrected line
             is_inserted = true
             break
         end
     end
-
-    result << i_arg unless is_inserted
+    result << i_arg if !is_inserted
 end
 
-puts result.join(" ")
+puts result
