c32608aeb2afe7dbb76cdf3fc46a27a78057bf5a
Sospeter <sosmogendi@gmail.com>
2023-08-24 08:01:22 -0700

    Printing a square in Javascript

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,23 +6,21 @@
     of the program.
 */
 
-if (process.argv.length <= 2) {
-    process.stderr.write("Missing argument\n");
-    process.stderr.write("Usage: ./1-print_square.js <size>\n");
-    process.stderr.write("Example: ./1-print_square.js 8\n");
+const process = require('process');  // Import the process module
+
+if (process.argv.length !== 3) {
+    console.error("Usage: ./1-print_square.js <size>");
+    console.error("Example: ./1-print_square.js 8");
     process.exit(1);
 }
 
-const size = parseInt(process.argv[2]);  // Parse size as a decimal number
+const size = parseInt(process.argv[2]);
 
-if (isNaN(size)) {
-    process.stderr.write("Invalid size\n");
+if (isNaN(size) || size <= 0) {
+    console.error("Invalid size");
     process.exit(1);
 }
 
 for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-        process.stdout.write("#");
-    }
-    process.stdout.write("\n");
+    console.log("#".repeat(size));
 }
